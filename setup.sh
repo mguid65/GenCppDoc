64bf4d1e27ce81c9d6f2dc0635153617f34f6770
Matthew Guidry <mguid2088@gmail.com>
2019-06-26 13:59:42 -0500

    Oops wrong line

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ rm -r clang_llvm_7.0.1
 mkdir clang_llvm_7.0.1
 cd clang_llvm_7.0.1
 
-wget http://releases.llvm.org/7.0.1/clang+llvm-7.0.1-x86_64-linux-sles11.3.tar.xz --strip-components=1
-tar -xvf clang+llvm-7.0.1-x86_64-linux-sles11.3.tar.xz
+wget http://releases.llvm.org/7.0.1/clang+llvm-7.0.1-x86_64-linux-sles11.3.tar.xz
+tar -xvf clang+llvm-7.0.1-x86_64-linux-sles11.3.tar.xz --strip-components=1
 
 wget http://releases.llvm.org/7.0.1/cfe-7.0.1.src.tar.xz
 tar -xvf cfe-7.0.1.src.tar.xz --strip-components=1
